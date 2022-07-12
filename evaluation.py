0c49bf00fdc105851612adbd6205626043638dbc
riccardobosio <81183336+riccardobosio@users.noreply.github.com>
2022-07-12 23:11:57 +0200

    added reranking feature to evaluation

--- evaluation.py
+++ evaluation.py
@@ -2,6 +2,7 @@ import argparse
 import numpy as np
 from datasets import load_metric, load_dataset
 from bertserini.BERTserini import BERTserini
+from bertserini.utils.rider import rerank
 
 
 def evaluate(model_path, num_val_examples):
@@ -13,13 +14,38 @@ def evaluate(model_path, num_val_examples):
     predicted_answers = []
     ground_truth = []
     for i, question in enumerate(val_examples):
-        print(f"Question number {i}:\n {question['question']}")
-        bertserini.retrieve(question, k=args.k)
-        answer = bertserini.answer(args.weight)
+        print(f"\nQuestion number {i}:\n {question['question']}")
+        if args.rider_ranking:
+          bertserini.retrieve(question, k=20)
+          all_contexts = bertserini.contexts
+          print('-------')
+          print('Contexts before re-ranking:')
+          for ctx in all_contexts[:args.k]:
+            print(ctx.text)
+          answer = bertserini.answer(args.weight)
+          print('-------')
+          print('Answer before re-ranking')
+          print(answer['prediction_text'])
+          all_answers = bertserini.all_answers
+          reranked_contexts = rerank(all_contexts, args.k, all_answers)
+          print('-------')
+          print('Contexts after re-ranking')
+          for ctx in reranked_contexts:
+            print(ctx.text)
+          bertserini.contexts = reranked_contexts
+          answer = bertserini.answer(args.weight)
+          print('-------')
+          print('Answer after re-ranking')
+          print(answer['prediction_text'])
+        else:
+          bertserini.retrieve(question, k=args.k)
+          answer = bertserini.answer(args.weight)
+
         predicted_answers.append(answer)
         ground_truth.append({"id": val_examples["id"][i], "answers": val_examples["answers"][i]})
+    
     result = metric.compute(predictions=predicted_answers, references=ground_truth)
-    print(f"These are the evaluation results: {result}")
+    print(f"\nThese are the evaluation results: {result}")
 
 
 if __name__ == "__main__":
@@ -29,6 +55,7 @@ if __name__ == "__main__":
     parser.add_argument('--weight', type=float, default=0.5, help="Weight used by the linear interpolation to compute final score.")
     parser.add_argument('--num_val_examples', type=int, help="Number of Squad validation examples to use.")
     parser.add_argument('--seed', type=int, default=42)
+    parser.add_argument('--rider_ranking', action='store_true', help="Whether to use or not contexts re-ranking")
     args = parser.parse_args()
 
     np.random.seed(args.seed)
